6d19f3c2e62a7370eac1292113b64d0840320ee6
Unknown <andreas@stickydirt.se>
2018-03-23 11:40:04 +0100

    Now rendering products in admin-site

--- www/js/classes/admin.class.js
+++ www/js/classes/admin.class.js
@@ -4,22 +4,34 @@ class Admin extends REST {
       this.getOrders({});
       this.openDetails();
       this.changeOrderStatus();
-      this.searchAdminOrders();   
-      this.clearSearch();   
+      this.searchAdminOrders();
+      this.clearSearch();
     }
 
     async getOrders(searchObj) {
-      this.orders = await Order.find(searchObj);   
+      this.orders = await Order.find(searchObj);
       $('.orderList').empty();
       for (const order of this.orders) {
         this.order = order;
         this.order.orderdate = this.order.orderdate.substring(0,10);
         this.orderRef = this.order._id.substring(18,24);
-        this.order.products.forEach(orderProduct => {
-          this.orderProductQuantity = orderProduct.quantity;
-          this.product = All.allProducts.find(product =>
-            product._id == orderProduct._id );
+
+        let productName = [];
+        let quantity = [];
+        this.order.products.forEach( async (product) => {
+          let kalle = All.allProducts.filter((o) => o._id == product._id);
+          productName.push(kalle[0]);
+          quantity.push(product.quantity);
         });
+
+        this.productItems = '';
+
+        for(let i = 0; i < productName.length; i++){
+          this.productItems += productName[i].title + ' (x' + quantity[i] + '), ';
+        }
+
+        this.productItems = this.productItems.substr(0, this.productItems.length -2);
+
         this.render('.orderList',2);
         this.render(`#progress-${this.order._id}`, 3);
         this.orderStatus(this.order.status);
@@ -39,9 +51,9 @@ class Admin extends REST {
     }
 
     clearSearch(){
-      let that = this;      
+      let that = this;
       $(document).on('click', '#clearSearch', function( event ) {
-        event.preventDefault();        
+        event.preventDefault();
         that.getOrders({});
         that.openDetails();
         that.changeOrderStatus();
@@ -52,19 +64,19 @@ class Admin extends REST {
 
     orderStatus(status){
       switch(status){
-        case 'Mottagen': 
+        case 'Mottagen':
           $(`#orderStatus-1-${this.order._id}`).addClass('d-block');
           break;
-        case 'Behandlad': 
+        case 'Behandlad':
           $(`#orderStatus-1-${this.order._id}`).addClass('d-block');
           $(`#orderStatus-2-${this.order._id}`).addClass('d-block');
           break;
-        case 'Skickad': 
+        case 'Skickad':
           $(`#orderStatus-1-${this.order._id}`).addClass('d-block');
           $(`#orderStatus-2-${this.order._id}`).addClass('d-block');
           $(`#orderStatus-3-${this.order._id}`).addClass('d-block');
           break;
-        case 'Levererad': 
+        case 'Levererad':
           $(`#orderStatus-1-${this.order._id}`).addClass('d-block');
           $(`#orderStatus-2-${this.order._id}`).addClass('d-block');
           $(`#orderStatus-3-${this.order._id}`).addClass('d-block');
@@ -91,27 +103,27 @@ class Admin extends REST {
     }
 
     async orderUpdate(idToChange, status){
-      this.orderToUpdate = this.orders.find( orderSelected => 
+      this.orderToUpdate = this.orders.find( orderSelected =>
         orderSelected._id == idToChange );
       this.orderToUpdate.status = status;
       return await this.orderToUpdate.save();
     }
-    
+
     searchAdminOrders () {
       let that = this;
       $(document).on('click', '#adminSearch', function (e) {
         e.preventDefault();
         let keyword = $('#adminKeyword').val();
-        that.searchEngineAdmin(keyword);  
-        $('#adminKeyword').val(''); 
-      });  
+        that.searchEngineAdmin(keyword);
+        $('#adminKeyword').val('');
+      });
     }
 
     searchEngineAdmin (keyword) {
       if (keyword) {
         this.searchResult = this.orders.find(order =>
           order._id.substring(18,24) == keyword
-        );      
+        );
         this.order = this.searchResult;
         this.order.orderdate = this.order.orderdate.substring(0,10);
         this.orderRef = this.order._id.substring(18,24);
@@ -120,7 +132,7 @@ class Admin extends REST {
         this.render('.orderList',2);
         this.render(`#progress-${this.searchResult._id}`, 3);
         this.orderStatus(this.searchResult.status);
-        $('#clearSearch').removeClass('d-none').fadeTo(500, 1); 
+        $('#clearSearch').removeClass('d-none').fadeTo(500, 1);
       }
     }
   }
